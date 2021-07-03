8a769b39b9e897d22fd7c7959ad1b6e79231a4b8
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-07-03 01:04:50 -0700

    Fixed JSC_MISSING_NULLABILITY_MODIFIER_JSDOC warning

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
  * Converts 24-hour time string to 12-hour time string.
  * @param {string} time The time string ("00:30", "01:45", "12:00", "22:15").
  * @return {string} Return converted 24-hour time string to 12-hour time.
- * @throws {Error} Will throw an error if the time string is invalid.
+ * @throws {!Error} Will throw an error if the time string is invalid.
  * @method
  * @example
  * convert('00:30'); // 12:30 AM
